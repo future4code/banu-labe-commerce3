4c71f48f3a9c0f1da11f4b12b1cd20f90755a3ee
Wellynton Benicio <well.03281@gmail.com>
2021-09-29 11:13:50 -0300

    Updates de terça-feira em grupo

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DivPrincipal = styled.div`
+    width: 100vw;
+    height: 30vh;
+    display: flex;
+    background-color: #000000;
+    color: #ffffff;
+`
+class  Footer extends React.Component {
+    render(){
+    return (
+        <DivPrincipal>
+                <h2>ASTROCOMMERCE</h2>  
+        </DivPrincipal>
+    )}
+};
+
+export default Footer; 
