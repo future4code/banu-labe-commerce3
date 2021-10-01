56c1124bc14e90058afbf0859aedaa203732140c
wellbenicio <wellynton.benicio@gmail.com>
2021-09-30 22:30:13 -0300

    Update componentes produtos

--- src/components/carrinho/Carrinho.jsx
+++ src/components/carrinho/Carrinho.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import Sidebar from './components/sidebar/Sidebar';
-import Footer from './components/footer/Footer';
-import Carrinho from '../../img/carrinho.png'
+import styled from 'styled-components';
 
-class  Sidebar extends React.Component {
+const DivPrincipal = styled.div `
+height: 100vh;
+display: flex;
+flex-direction: row-reverse;
+/* border: solid 1px blue; */
+justify-content: center;
+float: right;
+margin-right: 10px;
+margin-top: 20px;
+padding: 10px;
+width: 300px;
 
+`
+
+class Carrinho extends React.Component {
+
+    
     render(){
     return (
         <DivPrincipal>
             <div>
-                <img src={Carrinho} alt="imagem carrinho" />
+                <p>Estou aqui e sou o  carrinho</p>
             </div>
-            <Footer />
         </DivPrincipal>
     )}
 };
 
-export default Sidebar; 
-      
+export default Carrinho;
