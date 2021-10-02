3da9670f557457807b30fe97ae5f61c8f8b79fc6
Julia Zielke Rebellato <juliazrebellato@gmail.com>
2021-10-01 21:37:16 -0300

    primeiros filtros

--- src/components/Filtros/filtros.jsx
+++ src/components/Filtros/filtros.jsx
@@ -8,6 +8,16 @@ const FiltersContainer = styled.div `
     margin: 40px;
 `
 
+const OpcoesSort = styled.select`
+width: auto;
+padding: 2px;
+`
+
+const OpcoesOrder = styled.select`
+width: auto;
+padding: 2px;
+`
+
 export default function Filters(props) {
     return <FiltersContainer>
        <input
@@ -15,5 +25,37 @@ export default function Filters(props) {
           value={props.query}
           onChange={props.updateQuery}
           />    
+
+          <input
+         type="number"
+         placeholder="Preço mínimo"
+         value={props.minPrice}
+         onChange={props.updateMinPrice}
+        />
+
+        <input
+         type="number"
+         placeholder="Preço máximo"
+         value={props.maxPrice}
+         onChange={props.updateMaxPrice}
+        />
+        
+         <OpcoesSort
+            name="sort"
+            value={props.sortingParameter}
+            onChange={props.updateSortingParameter}
+         >
+            <option value="title">Título</option>
+            <option value="price">Preço</option>
+         </OpcoesSort>
+
+         <OpcoesOrder
+            name="sort"
+            value={props.order}
+            onChange={props.updateOrder}
+         >
+            <option value="asc">Crescente</option>
+            <option value="desc">Decrescente</option>
+         </OpcoesOrder>
     </FiltersContainer>
 }
